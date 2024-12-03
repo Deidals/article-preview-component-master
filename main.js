c0d25daad3dbbb020830a29deb39f0cfc4f996b5
Alqorni Trikandalas Ginting <alqorniginting@gmail.com>
2024-12-03 11:49:40 +0700

    Ver 1.00

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const button = document.querySelector(`button`);
 const footer = document.querySelector(`.interactive`);
 const buttonColor = document.querySelector(`#share`);
 const identity = document.querySelectorAll(`.identity`);
-const socialMedia = document.querySelectorAll(`.socialMedia`); // Change this to querySelectorAll
+const socialMedia = document.querySelectorAll(`.socialMedia`);
+
+const bubbleContainer = document.querySelector('.bubbleContainer');
+const children = bubbleContainer.querySelectorAll(':not(:last-child)');
+
+children.forEach(child => {
+  child.style.display = 'none';
+});
 
 socialMedia.forEach(item => {
-    item.style.display = `none`; // Initially hide all socialMedia items
+    item.style.display = `none`;
 });
 
 let isActive = false;
@@ -13,33 +20,53 @@ let isActive = false;
 button.addEventListener(`click`, function () {
     const screenWidth = window.innerWidth;
 
-    if (screenWidth < 1000 && isActive === false) {
-        footer.style.backgroundColor = `hsl(217, 19%, 35%)`;
-        button.style.backgroundColor = `hsl(214, 17%, 51%)`;
-        buttonColor.style.fill = `white`;
+    if (screenWidth < 1000) {
+        if(isActive === false){
+            footer.style.backgroundColor = `hsl(217, 19%, 35%)`;
+            button.style.backgroundColor = `hsl(214, 17%, 51%)`;
+            buttonColor.style.fill = `white`;
+    
+            identity.forEach(item => {
+                item.style.display = `none`;
+            });
+    
+            socialMedia.forEach(item => {
+                item.style.display = `block`;
+            });
+    
+            isActive = true;
+        } else if(isActive === true){
+            footer.style.backgroundColor = `inherit`;
+            button.style.backgroundColor = `hsl(210, 46%, 95%)`;
+            buttonColor.style.fill = `#6E8098`;
+    
+            identity.forEach(item => {
+                item.style.display = `block`;
+            });
+    
+            socialMedia.forEach(item => {
+                item.style.display = `none`;
+            });
+    
+            isActive = false;
+        }
+    } else{
+        if(isActive === false){
+            button.style.backgroundColor = `hsl(214, 17%, 51%)`;
+            buttonColor.style.fill = `white`;
 
-        identity.forEach(item => {
-            item.style.display = `none`;
-        });
+            children.forEach(item => {
+                item.style.display = `flex`;
+            });
+            isActive = true;
+        }else{
+            button.style.backgroundColor = `hsl(210, 46%, 95%)`;
+            buttonColor.style.fill = `#6E8098`;
 
-        socialMedia.forEach(item => {
-            item.style.display = `block`; // Hide social media icons
-        });
-
-        isActive = true;
-    } else if (screenWidth < 1000 && isActive === true) {
-        footer.style.backgroundColor = `inherit`;
-        button.style.backgroundColor = `hsl(210, 46%, 95%)`;
-        buttonColor.style.fill = `#6E8098`;
-
-        identity.forEach(item => {
-            item.style.display = `block`; // Show identity
-        });
-
-        socialMedia.forEach(item => {
-            item.style.display = `none`; // Show social media icons
-        });
-
-        isActive = false;
+            children.forEach(item => {
+                item.style.display = `none`;
+            });
+            isActive = false;
+        }
     }
 });
